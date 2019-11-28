0e2284f726088d4e97870425b05ddcb2ff2d9c4b
hulei <523387582@qq.com>
2019-11-28 13:14:25 +0800

    opt:错误收集优化

--- src/utils/monitor.js
+++ src/utils/monitor.js
@@ -1,75 +1,14 @@
 import Vue from 'vue';
 import monitor from 'monitor-web-sdk';
 
-let formatError = (errObj) => {
-	let col = errObj.column || errObj.columnNumber; // Safari Firefox
-	let row = errObj.line || errObj.lineNumber; // Safari Firefox
-	let message = errObj.message;
-	let name = errObj.name;
-
-	// 获取堆栈信息
-	let { stack } = errObj;
-	if (stack) {
-		let matchUrl = stack.match(/https?:\/\/[^\n]+/);
-		let urlFirstStack = matchUrl ? matchUrl[0] : '';
-		let regUrlCheck = /https?:\/\/(\S)*\.js/;
-
-		let resourceUrl = '';
-		if (regUrlCheck.test(urlFirstStack)) {
-			resourceUrl = urlFirstStack.match(regUrlCheck)[0];
-		}
-
-		let stackCol = null;
-		let stackRow = null;
-		let posStack = urlFirstStack.match(/:(\d+):(\d+)/);
-		if (posStack && posStack.length >= 3) {
-			[, stackCol, stackRow] = posStack;
-		}
-
-		return {
-			stack : stack,
-			col : Number(col || stackCol),
-			row : Number(row || stackRow),
-			message,
-			name,
-			resourceUrl
-		};
-	}
-
-	return {
-		row,
-		col,
-		message,
-		name
-	};
-};
-
-// js错误监控
-monitor.errorCatch.init(err => {
-	console.log('errorCatch', err);
+new monitor({
+	isErrorCatch : true, // js错误监控
+	isErrorResource : true, // 静态资源加载错误监控
+	isXhrHook : true, // 接口监控
+	isResources : true, // 资源加载耗时监控
+	isPerf : true, // 页面加载耗时监控
+	vue : Vue // vue 错误收集
+}).init(err => {
+	// 错误回调
+	console.log(err);
 });
-
-// 静态资源加载错误监控
-monitor.errorResource.init(err => {
-	console.log('errorResource', err);
-});
-
-// 接口监控
-monitor.xhrHook.init(data => {
-	console.log('xhrHook', data);
-});
-
-// 资源加载耗时监控
-monitor.resources.init(data => {
-	console.log('resources', data);
-});
-
-// 页面加载耗时监控
-monitor.perf.init(data => {
-	console.log('perf', data);
-});
-
-// vue错误捕捉
-Vue.config.errorHandler = (error, vm, info) => {
-	console.log({ type : 'vue_error', page : location.href, componentName : vm.$options ? vm.$options.name : '', content : info, ...formatError(error) });
-};
