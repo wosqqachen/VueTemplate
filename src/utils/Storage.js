5d78913d48d8bb5d386a6d06d0f0f6fd5ca8c093
hulei <523387582@qq.com>
2019-12-23 10:16:29 +0800

    opt:优化storage工具方法

--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -1,28 +1,39 @@
+/**
+ * type 0 localStorage, 1 sessionStorage , default 0
+ *   
+ */
 export default class Storage {
-	static get (key) {
-		let result = localStorage.getItem(key);
-		if (result) {
-			try {
-				return JSON.parse(result);
-			} catch (error) {
-				return result;
-			}
-		}
-		return null;
-	}
 
-	static set (key, value) {
-		if (value instanceof Object) {
-			value = JSON.stringify(value);
-		}
-		localStorage.setItem(key, value);
-	}
+  static PREFIXKEY = 'ADMIN_';
+  static types = ['localStorage','sessionStorage']
 
-	static delete (key) {
-		localStorage.removeItem(key);
-	}
+  static get (key,type=0) {
+  	let result = window[this.types[type]].getItem(`${this.PREFIXKEY}${key}`);
+  	if (result) {
+  		try {
+  			return JSON.parse(result);
+  		} catch (error) {
+  			return result;
+  		}
+  	}
+  	return null;
+  }
 
-	static isObjectEmpty (o) {
-		return Object.values(o).filter((v) => v !== '').length === 0;
-	}
+  static set (key, value,type=0) {
+  	if (value instanceof Object) {
+  		value = JSON.stringify(value);
+  	}
+  	window[this.types[type]].setItem(`${this.PREFIXKEY}${key}`, value);
+  }
+
+  static delete (key,type=0) {
+  	window[this.types[type]].removeItem(`${this.PREFIXKEY}${key}`);
+  }
+
+  static isObjectEmpty (o) {
+  	if (o instanceof Object) {
+  		return Object.values(o).filter((v) => v !== '').length === 0;
+  	}
+  	return true;
+  }
 }
